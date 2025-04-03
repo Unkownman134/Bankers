d81e0ffe1285f940e5b02e155feb798f2a7510d7
God_of_Admin <O0ohasho0O@proton.me>
2025-04-03 13:07:21 +0800

    BankersAlgorithmTest类完善

--- src/BankersAlgorithmTest.java
+++ src/BankersAlgorithmTest.java
@@ -2,11 +2,11 @@ import java.util.Scanner;
 
 public class BankersAlgorithmTest {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.println("请输入进程数：");
-        int n = sc.nextInt();
+        int n = scanner.nextInt();
         System.out.println("请输入资源种类数数：");
-        int m = sc.nextInt();
+        int m = scanner.nextInt();
 
         //初始化数据类
         BankData bankData = new BankData(n, m);
@@ -14,9 +14,31 @@ public class BankersAlgorithmTest {
         System.out.println("请输入系统"+m+"个资源可用数量：");
         int[] available = new int[m];
         for(int i=0;i<m;i++){
-            available[i]=sc.nextInt();
+            available[i]=scanner.nextInt();
         }
         bankData.setAvailable(available);
 
+        // 输入各进程的最大需求矩阵
+        System.out.println("请输入各进程的最大需求"+n+"*"+m+"矩阵:");
+        int[][] max = new int[n][m];
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < m; j++){
+                max[i][j] = scanner.nextInt();
+            }
+        }
+        bankData.setMax(max);
+
+        // 输入各进程的资源分配矩阵
+        System.out.println("请输入各进程的资源分配"+n+"*"+m+"矩阵:");
+        int[][] allocation = new int[n][m];
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < m; j++){
+                allocation[i][j] = scanner.nextInt();
+            }
+        }
+        bankData.setAllocation(allocation);
+
+        // 计算 Need 矩阵
+        bankData.computeNeed();
     }
 }
